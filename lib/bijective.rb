871d5b66294854348cecc77d4006567a631d27b5
Dennis Hall <dennis@dennishall.de>
2013-07-08 12:44:37 +0200

    Cleaned up code

--- lib/bijective.rb
+++ lib/bijective.rb
@@ -1,11 +1,32 @@
-module Bijective
-  class Generator
-    def self.generate_alphabet
-      upcase_letters   = ('A'..'Z').to_a
-      downcase_letters = ('a'..'z').to_a
-      numbers          = (0..9).to_a
+class Bijective
+  def self.generate_alphabet
+    upcase_letters   = ('A'..'Z').to_a
+    downcase_letters = ('a'..'z').to_a
+    numbers          = (0..9).to_a
 
-      alphabet = (upcase_letters + downcase_letters + numbers).shuffle.join
+    alphabet = (upcase_letters + downcase_letters + numbers).shuffle.join
+  end
+
+  def initialize alphabet
+    @alphabet = alphabet
+    @base = @alphabet.length
+  end
+
+  # returns string
+  def encode i
+    return @alphabet[0] if i == 0
+    s = ''
+    while i > 0
+      s << @alphabet[i.modulo(@base)]
+      i /= @base
     end
+    s.reverse
+  end
+
+  # returns integer
+  def decode s
+    i = 0
+    s.each_char { |c| i = i * @base + @alphabet.index(c) }
+    i
   end
 end
--- test/test_bijective.rb
+++ test/test_bijective.rb
@@ -2,10 +2,37 @@ require File.expand_path('../helper', __FILE__)
 
 class BijectiveTest < Test::Unit::TestCase
   def test_generate_alphabet
-    alphabet = Bijective::Generator.generate_alphabet
-    assert_equal 62, alphabet.split(//).uniq.join.length
+    alphabet = Bijective.generate_alphabet
+    assert_equal(62, alphabet.split(//).uniq.join.length)
 
-    alphabet2 = Bijective::Generator.generate_alphabet
-    assert_not_equal alphabet, alphabet2
+    alphabet2 = Bijective.generate_alphabet
+    assert_not_equal(alphabet, alphabet2)
+  end
+
+  def test_encode
+    set_alphabet
+    assert_equal('7',   @bijective.encode(0))
+    assert_equal('o',   @bijective.encode(10))
+    assert_equal('1U',  @bijective.encode(100))
+    assert_equal('pl',  @bijective.encode(1000))
+    assert_equal('3Cv', @bijective.encode(10000))
+    assert_equal('J7g', @bijective.encode(100000))
+  end
+
+  def test_decode
+    set_alphabet
+    assert_equal(0,      @bijective.decode('7'))
+    assert_equal(10,     @bijective.decode('o'))
+    assert_equal(100,    @bijective.decode('1U'))
+    assert_equal(1000,   @bijective.decode('pl'))
+    assert_equal(10000,  @bijective.decode('3Cv'))
+    assert_equal(100000, @bijective.decode('J7g'))
+  end
+
+private
+
+  def set_alphabet
+    alphabet = '713z6iE2lKokr5TLpHvwtQ9mZRJWcNbduBnqeCUfxDYGSXPVhj4asFyIgM8A0O'
+    @bijective = Bijective.new(alphabet)
   end
 end
