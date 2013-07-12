977fa838c84283400e9ce9c7582b7baef9e42783
Dennis Hall <dennis@dennishall.de>
2013-07-12 19:04:45 +0200

    Optimized test_bijective to use setup

--- test/test_bijective.rb
+++ test/test_bijective.rb
@@ -1,8 +1,12 @@
 require File.expand_path('../helper', __FILE__)
 
 class BijectiveTest < Test::Unit::TestCase
+  def setup
+    sequence = '713z6iE2lKokr5TLpHvwtQ9mZRJWcNbduBnqeCUfxDYGSXPVhj4asFyIgM8A0O'
+    @bijective = Bijective::Instance.new(sequence)
+  end
+
   def test_encode
-    set_sequence
     h = { '7'   => 0,
           'o'   => 10,
           '1U'  => 100,
@@ -13,7 +17,6 @@ class BijectiveTest < Test::Unit::TestCase
   end
 
   def test_decode
-    set_sequence
     h = { 0      => '7',
           10     => 'o',
           100    => '1U',
@@ -21,12 +24,5 @@ class BijectiveTest < Test::Unit::TestCase
           10000  => '3Cv',
           100000 => 'J7g' }
     h.each { |k, v| assert_equal(k, @bijective.decode(v)) }
-  end
-
-private
-
-  def set_sequence
-    sequence = '713z6iE2lKokr5TLpHvwtQ9mZRJWcNbduBnqeCUfxDYGSXPVhj4asFyIgM8A0O'
-    @bijective = Bijective::Instance.new(sequence)
   end
 end
